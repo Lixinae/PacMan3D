6ca243c1dee6b3175f67d73f1f5f88844a2456ce
Clément Dluzniewski <cdluznie@etud.u-pem.fr>
2017-11-20 18:42:20 +0100

    ajout classe position

---
+++ src/boardPosition.cpp
@@ -0,0 +1,21 @@
+#include "boardPosition.h"
+
+#include <iostream>
+
+using namespace std;
+
+BoardPosition::BoardPosition(int x, int y) : _x(x), _y(y) {
+
+}
+
+bool BoardPosition::operator==(const BoardPosition & other) const {
+	return (_x == other._x && _y == other._y);
+}
+
+bool BoardPosition::operator!=(const BoardPosition & other) const {
+	return not (*this == other);
+}
+
+ostream & operator<<(ostream & os, BoardPosition position) {
+	return (os << "(" << position._x << ", " << position._y << ")");
+}
