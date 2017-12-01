52f97d5bfa198d45171a618ff31948f8429a84e0
Vahen <naeos@laposte.net>
2017-12-01 16:09:57 +0100

    [UPD] Correctifs cmake
    Ajout de code de test dans le main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 
+#include <glimac/SDLWindowManager.hpp>
+#include <GL/glew.h>
 #include <iostream>
+#include <glimac/Program.hpp>
+#include <glimac/FilePath.hpp>
+#include <glimac/glm.hpp>
+#include <glimac/Sphere.hpp>
+#include <glimac/Image.hpp>
+#include <glimac/TrackballCamera.hpp>
+#include <glimac/FreeflyCamera.hpp>
+#include <cstddef>
+#include <SDL/SDL.h>
+
+using namespace glimac;
+using namespace std;
+using namespace glm;
 
 int main() {
-	std::cout << "test" << std::endl;
-	return 0;
-	
+    // Initialize SDL and open a window
+    float l=800,h=600;
+    SDLWindowManager windowManager(l,h, "GLImac");
+
+    // Initialize glew for OpenGL3+ support
+    GLenum glewInitError = glewInit();
+    if(GLEW_OK != glewInitError) {
+        std::cerr << glewGetErrorString(glewInitError) << std::endl;
+        return EXIT_FAILURE;
+    }
+
 } 
