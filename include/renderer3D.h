0cda72a62dcf50a00b578131a5153089c00aeba0
Clément Dluzniewski <cdluznie@etud.u-pem.fr>
2017-12-01 20:03:21 +0100

    renderer3D implemente renderer

--- include/renderer3D.h
+++ include/renderer3D.h
@@ -1,9 +1,10 @@
 #ifndef __RENDERER_3D_H__
 #define __RENDERER_3D_H__
 
+#include "renderer.h"
 #include "boardRepresentation.h"
 
-class Renderer3D {
+class Renderer3D : public Renderer {
 
 public:
 
