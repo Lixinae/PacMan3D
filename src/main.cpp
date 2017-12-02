3d6c9deb665bc3b35f3596848cb79f8c755b0716
Clément Dluzniewski <cdluznie@etud.u-pem.fr>
2017-12-02 14:30:32 +0100

    création du fichier json representant le jeu

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <glimac/SDLWindowManager.hpp>
 #include <GL/glew.h>
 #include <iostream>
+#include <fstream>
 #include <glimac/Program.hpp>
 #include <glimac/FilePath.hpp>
 #include <glimac/glm.hpp>
@@ -9,17 +10,16 @@
 #include <glimac/TrackballCamera.hpp>
 #include <glimac/FreeflyCamera.hpp>
 #include <cstddef>
-
 #include <Game.h>
 #include <Renderer.h>
 #include <Renderer3D.h>
-
 #include <json/json.hpp>
-using json = nlohmann::json;
 
 using namespace glimac;
 using namespace std;
 using namespace glm;
+
+using json = nlohmann::json;
 
 int main() {
 		
@@ -32,19 +32,12 @@ int main() {
         return EXIT_FAILURE;
     }
     
-    json jsonGame, jsonCases; 
-    jsonCases = json::array({
-		{{"position",{{"x",9}, {"y",10}}},{"case", {{"type","floor"}, {"args",json({})}}}},
-		{{"position",{{"x",10}, {"y",10}}},{"case", {{"type","floor"}, {"args",json({})}}}},
-		{{"position",{{"x",11}, {"y",10}}},{"case", {{"type","floor"}, {"args",json({})}}}}
-	});
-    jsonGame["pacman"]["position"]["x"] = 10;
-    jsonGame["pacman"]["position"]["y"] = 10;
-    jsonGame["pacman"]["orientation"] = "west";
-    jsonGame["board"]["cases"] = jsonCases;
-	
-	cout << setw(4) << jsonGame << endl;
-	
+    json jsonGame;
+    
+    ifstream gameFile("../game.json");
+    gameFile >> jsonGame; 
+	gameFile.close();
+
     Game game = Game::fromJSON(jsonGame);
     
     Renderer * renderer = new Renderer3D;
@@ -57,7 +50,7 @@ int main() {
                 done = true;
             }
         }
-        renderer->render(game.getRepresentation());
+		renderer->render(game.getRepresentation());
     }
     
     delete renderer;
