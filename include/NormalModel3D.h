bb2890f4d2b8308025ee3500af7d5678e2be5972
cdluznie <cdluznie@etud.u-pem.fr>
2017-12-10 19:59:53 +0100

    utilisation pour les modeles 3D de la classe Mesh

--- include/NormalModel3D.h
+++ include/NormalModel3D.h
@@ -2,6 +2,7 @@
 #define __NORMAL_MODEL_3D_H__
 
 #include <AbstractModel3D.h>
+#include <string>
 
 class NormalModel3D : public AbstractModel3D {
 
@@ -9,7 +10,7 @@ public:
 
 	static const string FRAGMENT_SHADER_NORMAL_3D;
 	
-	NormalModel3D();
+	NormalModel3D(const string & meshPath);
 
 };
 
