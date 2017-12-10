bc4e8a3d7dda776e8f4dbfd06d611e0714a0ab76
cdluznie <cdluznie@etud.u-pem.fr>
2017-12-10 20:03:02 +0100

    branchement conditionnel pour tester la differences des mesh

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -9,7 +9,14 @@ Mesh::Mesh(vector<ShapeVertex> vertices) : _vertices(vertices) {
 
 Mesh Mesh::fromOBJFile(const string & filePath) {
 	/* TODO real impl */
-	float r = 1;
+	float r;
+	if (filePath == "a") {
+		r = 0.7;
+	} else if (filePath == "b") {
+		r = 1;
+	} else {
+		r = 0.3;
+	}
 	Sphere sphere(r,16,16);
 	vector<ShapeVertex> vertices(sphere.getVertexCount());
 	for (int i = 0; i < sphere.getVertexCount(); i++) {
