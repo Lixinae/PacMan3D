b0b1b04ed4db94cd0b43290552a524b6257590c9
cdluznie <cdluznie@etud.u-pem.fr>
2017-12-23 18:31:02 +0100

    gestion des super pacgommes dans le json

--- src/Bonus.cpp
+++ src/Bonus.cpp
@@ -1,6 +1,7 @@
 #include <Bonus.h>
 
 #include <PacGomme.h>
+#include <SuperPacGomme.h>
 
 using json = nlohmann::json;
 
@@ -8,6 +9,9 @@ Bonus * Bonus::fromJSON(const json & jsonBonus) {
 	string type = jsonBonus["type"];
 	if (type == "pacgomme") {
 		return new PacGomme;
+	}
+	if (type == "super_pacgomme") {
+		return new SuperPacGomme;
 	}
 	throw invalid_argument(type + " is not a valid string representation of bonus type");
 }
