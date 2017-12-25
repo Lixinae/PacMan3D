b23e3cc37eb1fc0bdb6b00cf7f45a6bd763d842f
Vahen <naeos@laposte.net>
2017-12-25 16:00:33 +0100

    - Correction pour les commandes

--- src/EventHandler.cpp
+++ src/EventHandler.cpp
@@ -7,7 +7,6 @@ EventHandler::EventHandler(const map<control, SDLKey> & keyMap) : _keyMap(keyMap
 }
 
 bool EventHandler::handleEvent(SDLWindowManager & windowManager, Game & game) {
-	bool changeCamera = false;
 	SDL_Event event{};
 	while (windowManager.pollEvent(event)) {
 		if (event.type == SDL_QUIT) {
@@ -15,26 +14,36 @@ bool EventHandler::handleEvent(SDLWindowManager & windowManager, Game & game) {
 		}
 		if (event.type == SDL_KEYDOWN) {
 			if (event.key.keysym.sym == _keyMap[control::CHANGE_CAMERA]) {
-				changeCamera = true;
+				game.changeCamera();
+			}
+			if (event.key.keysym.sym == _keyMap[control::UP]){
+				game.orientPacman(Utils::Orientation::NORTH);
+			}
+			if (event.key.keysym.sym ==_keyMap[control::DOWN]){
+				game.orientPacman(Utils::Orientation::SOUTH);
+			}
+			if (event.key.keysym.sym == _keyMap[control::LEFT]){
+				game.orientPacman(Utils::Orientation::WEST);
+			}
+			if (event.key.keysym.sym == _keyMap[control::RIGHT]){
+				game.orientPacman(Utils::Orientation::EAST);
 			}
 		}
 	}
-	if (changeCamera) {
-		game.changeCamera();
-	}
+
 	//TODO not use is pressed but event type of sdl
-	if (windowManager.isKeyPressed(_keyMap[control::UP])) {
-		game.orientPacman(Utils::Orientation::NORTH);
-	}
-	if (windowManager.isKeyPressed(_keyMap[control::DOWN])) {
-		game.orientPacman(Utils::Orientation::SOUTH);
-	}
-	if (windowManager.isKeyPressed(_keyMap[control::LEFT])) {
-		game.orientPacman(Utils::Orientation::WEST);
-	}
-	if (windowManager.isKeyPressed(_keyMap[control::RIGHT])) {
-		game.orientPacman(Utils::Orientation::EAST);
-	}
+//	if (windowManager.isKeyPressed(_keyMap[control::UP])) {
+//		game.orientPacman(Utils::Orientation::NORTH);
+//	}
+//	if (windowManager.isKeyPressed(_keyMap[control::DOWN])) {
+//		game.orientPacman(Utils::Orientation::SOUTH);
+//	}
+//	if (windowManager.isKeyPressed(_keyMap[control::LEFT])) {
+//		game.orientPacman(Utils::Orientation::WEST);
+//	}
+//	if (windowManager.isKeyPressed(_keyMap[control::RIGHT])) {
+//		game.orientPacman(Utils::Orientation::EAST);
+//	}
 	if (windowManager.isKeyPressed(SDLK_b)) { // TODO should not be key
 		game.moveFrontCamera(1);
 	}
