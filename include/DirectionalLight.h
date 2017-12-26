8da48f641970086e1d4e3030cda0066b8c0ee77e
Vahen <naeos@laposte.net>
2017-12-26 01:33:18 +0100

    - Correctifs pour la lumiere directionnel

--- include/DirectionalLight.h
+++ include/DirectionalLight.h
@@ -13,24 +13,18 @@ class DirectionalLight : public Light {
 
 private:
     Light _baseLight;
-
     float _intensity;
     vec3 _direction;
-    vec3 _ambientColor;
-    vec3 _diffuseColor;
 
 
 public:
     DirectionalLight(const vec3 &color, float ambientIntensity, float diffuseIntensity,
-                     const vec3 &_direction, float intensity, const vec3 &ambientColor, const vec3 &diffuseColor);
+                     const vec3 &_direction, float intensity);
 
     float getIntensity() const;
 
     const vec3 &getDirection() const;
 
-    const vec3 &getAmbientColor() const;
-
-    const vec3 &getDiffuseColor() const;
 };
 
 
--- src/DirectionalLight.cpp
+++ src/DirectionalLight.cpp
@@ -2,13 +2,10 @@
 #include <DirectionalLight.h>
 
 DirectionalLight::DirectionalLight(const vec3 &color, float ambientIntensity, float diffuseIntensity,
-                                   const vec3 &direction, float intensity, const vec3 &ambientColor,
-                                   const vec3 &diffuseColor) :
+                                   const vec3 &direction, float intensity) :
         _baseLight(color, ambientIntensity, diffuseIntensity),
         _direction(direction),
-        _intensity(intensity),
-        _ambientColor(ambientColor),
-        _diffuseColor(diffuseColor) {
+        _intensity(intensity), {
 
 }
 
@@ -18,12 +15,4 @@ float DirectionalLight::getIntensity() const {
 
 const vec3 &DirectionalLight::getDirection() const {
     return _direction;
-}
-
-const vec3 &DirectionalLight::getAmbientColor() const {
-    return _ambientColor;
-}
-
-const vec3 &DirectionalLight::getDiffuseColor() const {
-    return _diffuseColor;
 }
